4001940e12f9c46dcf2c37ee2453d5cad3c63715
MS Munusamy <munusamymurugan1297@gmail.com>
2019-04-10 12:29:07 +0530

    Double Linked List Sorting Program

---
+++ Double_LL_Sorting/header.h
@@ -0,0 +1,25 @@
+/*************************************/
+/* Author      :Munusamy M           */
+/* Date        :09-04-2019           */
+/* Filename    :header.h             */
+/* Description :header file          */
+/*                                   */
+/*************************************/
+
+/* Included  header  files */
+  
+  #ifndef   HEADER_H		
+  #define   HEADER_H
+  #include  <stdio.h>
+  #include  <stdlib.h>
+
+/* Macro Definitions */
+  
+   void insert_node(int data,char *);
+
+/* user defined datatype definitions  */
+
+/* Function prototypes */
+ 
+   #endif /* HEADEER_H */
+
